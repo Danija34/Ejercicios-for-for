900ddd95f16948531ada63ae06a663c46d6ddbac
Daniel Ruiz <jhafet192@gmail.com>
2026-03-01 09:02:14 -0600

    Aqui se muestra una tabla de multiplicar del 1 al 5 (todas juntas)

--- Ejercicios for for/Program.cs
+++ Ejercicios for for/Program.cs
@@ -1,9 +1,10 @@
-﻿Console.WriteLine("Se le mostrara un trinagulo invertido en pantalla");
-for (int i = 5; i >= 1; i--)
+﻿Console.WriteLine("Se le mostrara una tabla de multiplicar del 1 al 5 en pantalla");
+for (int i = 1; i <= 5; i++)
 {
-    for (int j = 1; j <= i; j++)
+    for (int j = 1; j <= 5; j++)
     {
-    Console.Write("*");
+        int resultado = i * j;
+    Console.Write("{0,4}",resultado);
     }
     Console.WriteLine();
 }
