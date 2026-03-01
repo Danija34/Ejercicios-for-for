deaf6401c0405797c4f3f8da0b4a6630de396d21
Daniel Ruiz <jhafet192@gmail.com>
2026-03-01 09:22:59 -0600

    Aqui se le mostrara las posibles combinaciones del 1 al 3 en este ejercicio 7

--- Ejercicios for for/Program.cs
+++ Ejercicios for for/Program.cs
@@ -1,11 +1,9 @@
-﻿Console.WriteLine("Se le mostrara una matriz de 3x3 con numeros consecutivos del 1 al 9 en pantalla");
-int contador = 1;
-for (int i = 0; i < 3; i++)
+﻿Console.WriteLine("Se le mostrara las posibles combinaciones del 1 al 3");
+for (int i = 1; i <= 3; i++)
 {
-    for (int j = 0; j < 3; j++)
+    for (int j = 1; j <= 3; j++)
     {
-    Console.Write("{0,3}",contador);
-        contador++;
+    Console.Write(i+ "-"+j);
     }
     Console.WriteLine();
 }
