bb30b3bed6c84520d37600dd1c85925b7a2bed68
Daniel Ruiz <jhafet192@gmail.com>
2026-03-01 09:34:17 -0600

    En este ejercicio 9 se le mostrara un patron de numeros del 1 al 5

--- Ejercicios for for/Program.cs
+++ Ejercicios for for/Program.cs
@@ -1,9 +1,9 @@
-﻿Console.WriteLine("Se le mostrara una tabla de 4x4");
-for (int i = 1; i <= 4; i++)
+﻿Console.WriteLine("Se le mostrara un patron de numeros");
+for (int i = 1; i <= 5; i++)
 {
-    for (int j = 1; j <= 4; j++)
+    for (int j = 1; j <= 5; j++)
     {
-    Console.Write("{0,5}",i+",",+j);
+    Console.Write(i + "");
     }
     Console.WriteLine();
 }
