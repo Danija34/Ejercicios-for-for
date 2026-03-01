40cd4fc5300ff0ec7c2b48749f2bd6141a3a7a76
Daniel Ruiz <jhafet192@gmail.com>
2026-03-01 08:24:19 -0600

    Aqui esta el segundo  ejercicio de for anidado en la que se muestra en pantalla asteriscos

--- Ejercicios for for/Program.cs
+++ Ejercicios for for/Program.cs
@@ -1,10 +1,10 @@
 ﻿
-for (int i = 0; i < 5; i++)
+for (int i = 0; i < 4; i++)
 {
 
-    for (int j = 0; j < 5; j++)
+    for (int j = 0; j < 6; j++)
     {
-        Console.Write("1 ");
+        Console.Write('*');
     }
     Console.WriteLine();
 }
